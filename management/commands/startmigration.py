d4967d6d1feac4e60fbb4bd3500cc5e8503ad024
andrew@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <>
2008-07-30 15:57:18 +0000

    Magical migration creation situations!

--- management/commands/startmigration.py
+++ management/commands/startmigration.py
@@ -12,16 +12,23 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list
     help = "Creates a new template migration for the given app"
 
-    def handle(self, app=None, name="", **options):
+    def handle(self, app=None, name="", model=None, **options):
         if not app:
             print "Please provide an app in which to create the migration."
             return
         # See if the app exists
         try:
-            app_module = __import__(app, {}, {}, ['migrations'])
+            app_module = __import__(app, {}, {}, ['migrations','models'])
         except ImportError:
             print "App '%s' doesn't seem to exist." % app
             return
+        # If there's a model, open it and have a poke
+        if model:
+            try:
+                model = getattr(app_module.models, model)
+                print model
+            except AttributeError:
+                print "The specified model '%s' doesn't seem to exist." % model
         # Make the migrations directory if it's not there
         migrations_dir = os.path.join(
             os.path.dirname(app_module.__file__),
@@ -47,6 +54,42 @@ class Command(BaseCommand):
             "".join([random.choice(string.letters.lower()) for i in range(0)]), # Possible random stuff insertion
             name,
         )
+        # If there's a model, make the migration skeleton, else leave it bare
+        if model:
+            table_name = model._meta.db_table
+            fields = []
+            for f in model._meta.local_fields:
+                # We use a list of tuples to get nice ordering
+                field = [
+                    ("name", f.column),
+                    ("null", f.null),
+                ]
+                type, type_param = genericify_type(f.db_type())
+                field.append(("type", type))
+                field.append(("type_param", type_param))
+                if f.primary_key:
+                    field.append(('primary', True))
+                if f.unique:
+                    field.append(('unique', True))
+                if f.rel:
+                    field.append(('related_to', (
+                        f.rel.to._meta.db_table,
+                        f.rel.to._meta.get_field(f.rel.field_name).column,
+                    )))
+                fields.append(field)
+            forwards = '''db.create_table("%s", [
+            %s
+        ])''' % (
+                table_name,
+                ",\n            ".join([
+                    "{%s}" % ", ".join("%r: %r" % (x, y) for x, y in f)
+                    for f in fields
+                ]),
+            )
+            backwards = '''db.delete_table("%s")''' % table_name
+        else:
+            forwards = '"Write your forwards migration here"'
+            backwards = '"Write your backwards migration here"'
         fp = open(os.path.join(migrations_dir, new_filename), "w")
         fp.write("""
 from south.db import db
@@ -55,10 +98,23 @@ from %s.models import *
 class Migration:
     
     def forwards(self):
-        # Write your forwards migration here
-        pass
+        %s
     
     def backwards(self):
-        # Write your backwards migration here
-        pass""" % app)
+        %s
+""" % (app, forwards, backwards))
         fp.close()
+
+
+def genericify_type(typestr):
+    if "(" not in typestr:
+        type = typestr
+        param = None
+    else:
+        type, param = typestr.split("(")
+        param = param[:-1]
+    # Make sure it doesn't need to be mapped back to a more generic type
+    type = {
+        "varchar": "string",
+    }.get(type, type)
+    return type, param
