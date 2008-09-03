2d53102b0ad86058909c3bd2acf083fc3a23dd38
andrew@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <none@none>
2008-09-03 21:21:32 +0000

    Version bump in preparation for release. Also, a contender in "shortest commit ever".
    --HG--
    extra : convert_revision : svn%3A69d324d9-c39d-4fdc-8679-7745eae9e2c8/trunk%4053

--- __init__.py
+++ __init__.py
@@ -2,5 +2,5 @@
 South - Useable migrations for Django apps
 """
 
-__version__ = "0.2"
+__version__ = "0.3"
 __authors__ = ["Andrew Godwin <andrew@aeracode.org>", "Andy McCurdy <andy@andymccurdy.com>"]
