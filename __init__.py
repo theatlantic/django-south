3ffea45c3995a04d36b827e5445ac63052fef33b
amccurdy@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <none@none>
2008-09-03 20:57:24 +0000

    shameless author update
    --HG--
    extra : convert_revision : svn%3A69d324d9-c39d-4fdc-8679-7745eae9e2c8/trunk%4052

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ South - Useable migrations for Django apps
 """
 
 __version__ = "0.2"
-__authors__ = ["Andrew Godwin <andrew@aeracode.org>"]
+__authors__ = ["Andrew Godwin <andrew@aeracode.org>", "Andy McCurdy <andy@andymccurdy.com>"]
