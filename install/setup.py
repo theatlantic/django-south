4f920add98c456bdbf607f3fe80881d77e97781d
andrew@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <>
2008-09-15 12:25:38 +0000

    Setup.py

---
+++ install/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='South',
+    version='0.3',
+    description='South: Migrations for Django',
+    author='Andrew Godwin & Andy McCurdy',
+    author_email='south@aeracode.org',
+    url='http://south.aeracode.org/',
+    packages="south",
+)
