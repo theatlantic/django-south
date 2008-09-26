8c58aff7b16dc00cf5ec959ccedc5207240c9c4a
andrew@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <none@none>
2008-09-26 14:29:24 +0000

    Tests now don't output anything.
    --HG--
    extra : convert_revision : svn%3A69d324d9-c39d-4fdc-8679-7745eae9e2c8/trunk%4074

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,11 @@
 
-from south.tests.db import *
-from south.tests.logic import *
+from django.conf import settings
+
+try:
+    skiptest = settings.SKIP_SOUTH_TESTS
+except:
+    skiptest = False
+
+if not skiptest:
+    from south.tests.db import *
+    from south.tests.logic import *
