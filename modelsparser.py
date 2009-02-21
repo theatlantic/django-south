a2bbef8f2e3fcbb25266b1ec39f918b4f7f145c7
andrew@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <none@none>
2009-02-21 19:42:45 +0000

    Models.py parser is in a state of OMG-it-actually-works.
    --HG--
    extra : convert_revision : svn%3A69d324d9-c39d-4fdc-8679-7745eae9e2c8/trunk%40138

--- modelsparser.py
+++ modelsparser.py
@@ -8,6 +8,7 @@ import inspect
 import parser
 import symbol
 import token
+import keyword
 
 
 def name_that_thing(thing):
@@ -17,7 +18,7 @@ def name_that_thing(thing):
             return "symbol.%s" % name
     for name in dir(token):
         if getattr(token, name) == thing:
-            return "symbol.%s" % name
+            return "token.%s" % name
     return str(thing)
 
 
@@ -48,7 +49,6 @@ class STTree(object):
     "A syntax tree wrapper class."
     
     def __init__(self, tree):
-        assert isinstance(tree, tuple), "You must pass in a tree tuple."
         self.tree = tree
     
     
@@ -57,15 +57,23 @@ class STTree(object):
         return self.tree[0]
     
     
-    def tree_walk(self, recursive=True):
-        "Yields (symbol, subtree) for the entire subtree."
+    @property
+    def value(self):
+        return self.tree
+    
+    
+    def flatten(self, recursive=True):
+        """
+        Yields (symbol, subtree) for the entire subtree.
+        Comes out in reverse lexical order, until my brain unmelts enough.
+        """
         stack = [self.tree]
         done_outer = False
         while stack:
             atree = stack.pop()
             if isinstance(atree, tuple):
                 if recursive or done_outer:
-                    yield atree[0], atree
+                    yield atree[0], STTree(atree)
                 if recursive or not done_outer:
                     for bit in atree[1:]:
                         stack.append(bit)
@@ -74,9 +82,9 @@ class STTree(object):
     
     def findAllType(self, ntype, recursive=True):
         "Returns all nodes with the given type in the tree."
-        for symbol, subtree in self.tree_walk(recursive=recursive):
+        for symbol, subtree in self.flatten(recursive=recursive):
             if symbol == ntype:
-                yield STTree(subtree)
+                yield subtree
     
     
     def find(self, selector):
@@ -92,10 +100,15 @@ class STTree(object):
             # Split up the parts
             parts = re.split(r'(?:[\s]|(>))+', selector)
             # Take the first part, use it for results
-            subresults = list(self.findAllType(thing_that_name(parts[0])))
+            if parts[0] == "^":
+                subresults = [self]
+            else:
+                subresults = list(self.findAllType(thing_that_name(parts[0])))
             recursive = True
             # For each remaining part, do something
             for part in parts[1:]:
+                if not subresults:
+                    break
                 if part == ">":
                     recursive = False
                 elif not part:
@@ -119,15 +132,111 @@ class STTree(object):
     __repr__ = __str__
     
 
-
 def get_model_tree(model_instance):
+    # Get the source of the model's file
+    source = open(inspect.getsourcefile(model_instance)).read()
+    tree = STTree(parser.suite(source).totuple())
     
-    # Get the source of the model
-    source = "".join(inspect.getsource(model_instance))
-    return STTree(parser.suite(source).totuple())
+    # Now, we have to find it
+    for poss in tree.find("compound_stmt"):
+        if poss.value[1][0] == symbol.classdef and \
+           poss.value[1][2][1] == model_instance.__name__:
+            # This is the tree
+            return poss
+
+
+token_map = {
+    token.DOT: ".",
+    token.LPAR: "(",
+    token.RPAR: ")",
+    token.EQUAL: "=",
+    token.EQEQUAL: "==",
+    token.COMMA: ",",
+    token.LSQB: "[",
+    token.RSQB: "]",
+    token.AMPER: "&",
+    token.BACKQUOTE: "`",
+    token.CIRCUMFLEX: "^",
+    token.CIRCUMFLEXEQUAL: "^=",
+    token.COLON: ":",
+    token.DOUBLESLASH: "//",
+    token.DOUBLESLASHEQUAL: "//=",
+    token.DOUBLESTAR: "**",
+    token.DOUBLESLASHEQUAL: "**=",
+    token.GREATER: ">",
+    token.LESS: "<",
+    token.GREATEREQUAL: ">=",
+    token.LESSEQUAL: "<=",
+    token.LBRACE: "{",
+    token.RBRACE: "}",
+    token.SEMI: ";",
+    token.PLUS: "+",
+    token.MINUS: "-",
+    token.STAR: "*",
+    token.SLASH: "/",
+    token.VBAR: "|",
+    token.PERCENT: "%",
+    token.TILDE: "~",
+    token.AT: "@",
+    token.NOTEQUAL: "!=",
+    token.LEFTSHIFT: "<<",
+    token.RIGHTSHIFT: ">>",
+    token.LEFTSHIFTEQUAL: "<<=",
+    token.RIGHTSHIFTEQUAL: ">>=",
+    token.PLUSEQUAL: "+=",
+    token.MINEQUAL: "-=",
+    token.STAREQUAL: "*=",
+    token.SLASHEQUAL: "/=",
+    token.VBAREQUAL: "|=",
+    token.PERCENTEQUAL: "%=",
+    token.AMPEREQUAL: "&=",
+}
+
+
+def reform(bits):
+    "Returns the string that the list of tokens/symbols 'bits' represents"
+    output = ""
+    for bit in bits:
+        if bit in token_map:
+            output += token_map[bit]
+        elif bit[0] in [token.NAME, token.STRING, token.NUMBER]:
+            if keyword.iskeyword(bit[1]):
+                output += " %s " % bit[1]
+            else:
+                output += bit[1]
+    return output
+
+
+def extract_field(tree):
+    # Collapses the tree and tries to parse it as a field def
+    bits = []
+    for sym, subtree in reversed(list(tree.flatten())):
+        if sym in token_map:
+            bits.append(sym)
+        elif sym == token.NAME:
+            bits.append(subtree.value)
+        elif sym == token.STRING:
+            bits.append(subtree.value)
+        elif sym == token.NUMBER:
+            bits.append(subtree.value)
+    # Check it looks right
+    if len(bits) < 2 or bits[1] != token.EQUAL:
+        return
+    # OK, extract and reform it
+    return bits[0][1], reform(bits[2:])
+    
 
 
 def get_model_fields(model_instance):
     
-    print tree.find("suite expr_stmt")
+    tree = get_model_tree(model_instance)
+    possible_field_defs = tree.find("^ > classdef > suite > stmt > simple_stmt > small_stmt > expr_stmt")
+    fields = {}
+    
+    for pfd in possible_field_defs:
+        field = extract_field(pfd)
+        if field:
+            fields[field[0]] = field[1]
+    
+    return fields
 
