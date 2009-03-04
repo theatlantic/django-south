71ce0286e45b9493dda8061b2b99ab9669bb9c76
andrew@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <none@none>
2009-03-04 23:57:23 +0000

    Updated ORM and ORM-ified migration command; it's nearly there.
    --HG--
    extra : convert_revision : svn%3A69d324d9-c39d-4fdc-8679-7745eae9e2c8/trunk%40141

--- management/commands/startmigration2.py
+++ management/commands/startmigration2.py
@@ -6,6 +6,11 @@ from django.contrib.contenttypes.generic import GenericRelation
 from django.db.models.fields import FieldDoesNotExist
 from optparse import make_option
 
+try:
+    set
+except NameError:
+    from sets import Set as set
+
 from south import migration, modelsparser
 
 import sys
@@ -24,10 +29,12 @@ class Command(BaseCommand):
             help='Generate an Add Column migration for the specified modelname.fieldname - you can use this multiple times to add more than one column.'),
         make_option('--initial', action='store_true', dest='initial', default=False,
             help='Generate the initial schema for the app.'),
+        make_option('--freeze', action='append', dest='freeze_list', type='string',
+            help='Freeze the specified model(s). Pass in either an app name (to freeze the whole app) or a single model, as appname.modelname.'),
     )
     help = "Creates a new template migration for the given app"
     
-    def handle(self, app=None, name="", model_list=None, field_list=None, initial=False, **options):
+    def handle(self, app=None, name="", model_list=None, field_list=None, initial=False, freeze_list=None, **options):
         
         # If model_list is None, then it's an empty list
         model_list = model_list or []
@@ -141,54 +148,176 @@ class Command(BaseCommand):
         # Initialise forwards, backwards and models to blank things
         forwards = ""
         backwards = ""
-        frozen_models = {} # Frozen models, used by the Fake ORM
+        frozen_models = set() # Frozen models, used by the Fake ORM
+        stub_models = set() # Frozen models, but only enough for relation ends (old mock models)
+        
+        # Add anything frozen (I almost called the dict Iceland...)
+        if freeze_list:
+            for item in freeze_list:
+                if "." in item:
+                    # It's a specific model
+                    app_name, model_name = item.split(".", 1)
+                    model = models.get_model(models.get_app(app_name), model_name)
+                    frozen_models.add(model)
+                else:
+                    # Get everything in an app!
+                    frozen_models.update(models.get_models(models.get_app(item)))
+        
+        # Add fields
+        if fields_to_add:
+            for model, field_name, field in fields_to_add:
+                
+                if isinstance(field, models.ManyToManyField):
+                    # Add a stub model for each side
+                    stub_models.add(model)
+                    stub_models.add(field.rel.to)
+                    # And a field defn, that's actually a table creation
+                    forwards += CREATE_M2MFIELD_SNIPPET % (
+                        model._meta.object_name,
+                        field.name,
+                        field.m2m_db_table(),
+                        field.m2m_column_name()[:-3], # strip off the '_id' at the end
+                        model._meta.object_name,
+                        field.m2m_reverse_name()[:-3], # strip off the '_id' at the ned
+                        field.rel.to._meta.object_name
+                        )
+                    backwards += DELETE_M2MFIELD_SNIPPET % (
+                        model._meta.object_name,
+                        field.name,
+                        field.m2m_db_table()
+                    )
+                    continue
+                
+                # Add any dependencies
+                stub_models.update(field_dependencies(field))
+                
+                # Work out the definition
+                triple = modelsparser.get_model_fields(model)[field_name]
+                field_definition = make_field_constructor(app, field, triple)
+                
+                forwards += CREATE_FIELD_SNIPPET % (
+                    model._meta.object_name,
+                    field.name,
+                    model._meta.db_table,
+                    field.name,
+                    field_definition,
+                )
+                backwards += DELETE_FIELD_SNIPPET % (
+                    model._meta.object_name,
+                    field.name,
+                    model._meta.db_table,
+                    field.column,
+                )
         
         # Generate model migrations
         if models_to_migrate:
             for model in models_to_migrate:
-                # Add the model to the frozen list
-                frozen_models["%s.%s" % (app, model._meta.object_name)] = model
+                # Add the model's dependencies to the stubs
+                stub_models.update(model_dependencies(model))
+                # Get the field definitions
+                fields = modelsparser.get_model_fields(model)
+                # Turn the (class, args, kwargs) format into a string
+                for field, triple in fields.items():
+                    fields[field] = make_field_constructor(
+                        app,
+                        model._meta.get_field_by_name(field)[0],
+                        remove_useless_attributes(triple),
+                    )
+                # Make the code
+                forwards += CREATE_TABLE_SNIPPET % (
+                    model._meta.object_name,
+                    model._meta.db_table,
+                    "\n            ".join(["('%s', %s)," % (fname, fdef) for fname, fdef in fields.items()]),
+                )
+                # And the backwards code
+                backwards += DELETE_TABLE_SNIPPET % (
+                    model._meta.object_name, 
+                    model._meta.db_table
+                )
         
         # Default values for forwards/backwards
         if (not forwards) and (not backwards):
             forwards = '"Write your forwards migration here"'
             backwards = '"Write your backwards migration here"'
         
+        all_models = {}
+        
         # Fill out frozen model definitions
-        for key, model in frozen_models.items():
-            frozen_models[key] = remove_useless_attributes(
-                modelsparser.get_model_fields(model)
-            )
+        for model in frozen_models:
+            fields = modelsparser.get_model_fields(model)
+            # Remove useless attributes (like 'choices')
+            for name, field in fields.items():
+                fields[name] = remove_useless_attributes(field)
+            # See if there's a Meta
             meta = modelsparser.get_model_meta(model)
             if meta:
-                frozen_models[key]['Meta'] = meta
+                fields['Meta'] = meta
+            # Add it to our models
+            all_models[model_key(model)] = fields
         
+        # Fill out stub model definitions
+        for model in stub_models:
+            if model in frozen_models:
+                continue # We'd rather use full models than stubs.
+            fields = modelsparser.get_model_fields(model)
+            # Now, take only the PK (and a 'we're a stub' field) and freeze 'em
+            pk = model._meta.pk.get_attname()
+            fields = {
+                pk: remove_useless_attributes(fields[pk]),
+                "_stub": True,
+            }
+            # Meta is important too.
+            meta = modelsparser.get_model_meta(model)
+            if meta:
+                fields['Meta'] = meta
+            # Add it to the models
+            all_models[model_key(model)] = fields
+        
+        # Write the migration file
         fp = open(os.path.join(migrations_dir, new_filename), "w")
-        fp.write("""%s
-from south.db import db
-from django.db import models
-from %s.models import *
-
-class Migration:
-    
-    def forwards(self, orm):
-        %s
-    
-    def backwards(self, orm):
-        %s
-    
-    models = %s
-""" % (encoding or "", '.'.join(app_module_path), forwards, backwards, pprint_frozen_models(frozen_models)))
+        fp.write(MIGRATION_SNIPPET % (
+            encoding or "", '.'.join(app_module_path), 
+            forwards, 
+            backwards, 
+            pprint_frozen_models(all_models)
+        ))
         fp.close()
         print "Created %s." % new_filename
 
 
+### Module handling functions
+
+def model_key(model):
+    "For a given model, return 'appname.modelname'."
+    return ("%s.%s" % (model._meta.app_label, model._meta.object_name)).lower()
+
+
+### Dependency resolvers
+
+def model_dependencies(model):
+    """
+    Returns a set of models this one depends on to be defined; things like
+    OneToOneFields as ID, ForeignKeys everywhere, etc.
+    """
+    depends = set()
+    for field in model._meta.fields:
+        depends.update(field_dependencies(field))
+    return depends
+
+def field_dependencies(field):
+    depends = set()
+    if isinstance(field, (models.OneToOneField, models.ForeignKey)):
+        depends.add(field.rel.to)
+    return depends
+    
+
+
 ### Prettyprinters
 
-def pprint_frozen_models(frozen_models):
+def pprint_frozen_models(models):
     return "{\n        %s\n    }" % ",\n        ".join([
         "%r: %s" % (name, pprint_fields(fields))
-        for name, fields in frozen_models.items()
+        for name, fields in models.items()
     ])
 
 def pprint_fields(fields):
@@ -200,9 +329,87 @@ def pprint_fields(fields):
 
 ### Output sanitisers
 
-def remove_useless_attributes(fields):
-    for name, field in fields.items():
-        # If that has a 'choices' attribute, remove it.
-        if "choices" in field[2]:
-            del fields[name][2]['choices']
-    return fields
+def remove_useless_attributes(field):
+    # If that has a 'choices' attribute, remove it.
+    if "choices" in field[2]:
+        del field[2]['choices']
+    return field
+
+
+### Turns (class, args, kwargs) triples into function defs.
+
+def make_field_constructor(default_app, field, triple):
+    if hasattr(field, "rel") and hasattr(field.rel, "to") and field.rel.to:
+        rel_to = field.rel.to
+    else:
+        rel_to = None
+    args = [poss_ormise(default_app, rel_to, arg) for arg in triple[1]]
+    kwds = ["%s=%s" % (k, poss_ormise(default_app, rel_to, v)) for k,v in triple[2].items()]
+    return "%s(%s)" % (triple[0], ", ".join(args+kwds))
+
+def poss_ormise(default_app, rel_to, arg):
+    """
+    Given the name of something that needs orm. stuck on the front and
+    a python eval-able string, possibly add orm. to it.
+    """
+    # If it's not a relative field, short-circuit out
+    if not rel_to:
+        return arg
+    # Get the name of the other model
+    rel_name = rel_to._meta.object_name
+    # Is it in a different app? If so, use proper addressing.
+    if rel_to._meta.app_label != default_app:
+        real_name = "orm['%s.%s']" % (default_app, rel_name)
+    else:
+        real_name = "orm.%s" % rel_name
+    # Now see if we can replace it.
+    if arg == rel_name:
+        return real_name
+    return arg
+
+
+### Various code snippets we need to use
+
+MIGRATION_SNIPPET = """%s
+from south.db import db
+from django.db import models
+from %s.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        %s
+    
+    
+    def backwards(self, orm):
+        %s
+    
+    
+    models = %s
+"""
+CREATE_TABLE_SNIPPET = '''
+        # Model '%s'
+        db.create_table(%r, (
+            %s
+        ))'''
+DELETE_TABLE_SNIPPET = '''
+        # Model '%s'
+        db.delete_table(%r)'''
+CREATE_FIELD_SNIPPET = '''
+        # Adding field '%s.%s'
+        db.add_column(%r, %r, %s)
+        '''
+DELETE_FIELD_SNIPPET = '''
+        # Deleting field '%s.%s'
+        db.delete_column(%r, %r)
+        '''
+CREATE_M2MFIELD_SNIPPET = '''
+        # Adding ManyToManyField '%s.%s'
+        db.create_table('%s', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('%s', models.ForeignKey(%s, null=False)),
+            ('%s', models.ForeignKey(%s, null=False))
+        )) '''
+DELETE_M2MFIELD_SNIPPET = '''
+        # Dropping ManyToManyField '%s.%s'
+        db.drop_table('%s')'''
