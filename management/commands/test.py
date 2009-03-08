98c6d3b54fb6f7f76a44d8e12fc96fa1258e3f02
andrew@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <>
2009-03-08 17:29:05 +0000

    Last commit was missing a 'not'.

--- management/commands/test.py
+++ management/commands/test.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class Command(test.Command):
     
     def handle(self, *args, **kwargs):
-        if not hasattr(settings, "SOUTH_TESTS_MIGRATE") or settings.SOUTH_TESTS_MIGRATE:
+        if not hasattr(settings, "SOUTH_TESTS_MIGRATE") or not settings.SOUTH_TESTS_MIGRATE:
             # point at the core syncdb command when creating tests
             # tests should always be up to date with the most recent model structure
             management.get_commands()
