181b20ccf989773ebbbb263351658f4529f99f71
andrew@69d324d9-c39d-4fdc-8679-7745eae9e2c8 <>
2009-04-03 09:37:27 +0000

    Add warning about [197].

--- __init__.py
+++ __init__.py
@@ -3,6 +3,4 @@ Temporary South module while we move directory structure.
 """
 import sys, os
 sys.path.insert(0, os.path.dirname(__file__))
-print >> sys.stderr, "Depreciation warning: South has now moved to the south/ subdirectory. You will need to reconfigure your svn:external or library paths in your application."
-del sys.modules['south']
-import south
+raise DeprecationWarning("South has now moved to the south/ subdirectory. You will need to reconfigure your svn:external or library paths in your application. See http://south.aeracode.org/wiki/DirectoryMove for details.")
