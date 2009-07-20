9f2c3f83b37f92a500a43eaa0d8ad924b9e61771
Andrew Godwin <andrew@aeracode.org>
2009-07-20 19:21:27 +0100

    Remove DepreciationWarning

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Temporary South module while we move directory structure.
-"""
-import sys, os
-sys.path.insert(0, os.path.dirname(__file__))
-raise DeprecationWarning("South has now moved to the south/ subdirectory. You will need to reconfigure your svn:external or library paths in your application. See http://south.aeracode.org/wiki/DirectoryMove for details.")
