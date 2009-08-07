9e1f27460ecc61d7d89cb7dcb2c42cb6a56b1e93
Simon Law <sfllaw@sfllaw.ca>
2009-08-07 00:18:22 -0400

    Split out get_migrator() and LoadInitialDataMigrator from migrate_app()

--- south/migration/migrators.py
+++ south/migration/migrators.py
@@ -1,8 +1,12 @@
 from copy import copy
+from cStringIO import StringIO
 import datetime
 import inspect
 import sys
 import traceback
+
+from django.core.management import call_command
+from django.db import models
 
 from south import exceptions
 from south.db import db
@@ -14,12 +18,21 @@ class Migrator(object):
     def __init__(self, verbosity=0):
         self.verbosity = int(verbosity)
 
+    @staticmethod
+    def title(target):
+        raise NotImplementedError()
+        
+    @staticmethod
+    def status(target):
+        raise NotImplementedError()
+
     def print_status(self, migration):
         status = self.status(migration)
         if self.verbosity and status:
             print status
 
-    def orm(self, migration):
+    @staticmethod
+    def orm(migration):
         raise NotImplementedError()
 
     def backwards(self, migration):
@@ -28,15 +41,6 @@ class Migrator(object):
     def direction(self, migration):
         raise NotImplementedError()
 
-    def print_backwards(self, migration):
-        old_debug, old_dry_run = db.debug, db.dry_run
-        db.debug = db.dry_run = True
-        try:
-            self.backwards(migration)()
-        except:
-            db.debug, db.dry_run = old_debug, old_dry_run
-            raise
-
     @staticmethod
     def _wrap_direction(direction, orm):
         args = inspect.getargspec(direction)
@@ -44,6 +48,21 @@ class Migrator(object):
             # Old migration, no ORM should be passed in
             return direction
         return (lambda: direction(orm))
+
+    @staticmethod
+    def record(migration):
+        raise NotImplementedError()
+
+    def run_migration_error(self, migration, extra_info=''):
+        return (' ! Error found during real run of migration! Aborting.\n'
+                '\n'
+                ' ! Since you have a database that does not support running\n'
+                ' ! schema-altering statements in transactions, we have had \n'
+                ' ! to leave it in an interim state between migrations.\n'
+                '%s\n'
+                ' ! The South developers regret this has happened, and would\n'
+                ' ! like to gently persuade you to consider a slightly\n'
+                ' ! easier-to-deal-with DBMS.\n') % extra_info
 
     def run_migration(self, migration):
         migration_function = self.direction(migration)
@@ -54,19 +73,7 @@ class Migrator(object):
         except:
             db.rollback_transaction()
             if not db.has_ddl_transactions:
-                print ' ! Error found during real run of migration! Aborting.'
-                print
-                print ' ! Since you have a database that does not support running'
-                print ' ! schema-altering statements in transactions, we have had to'
-                print ' ! leave it in an interim state between migrations.'
-                if self.torun == 'forwards':
-                    print
-                    print " ! You *might* be able to recover with:"
-                    self.print_backwards(migration)
-                print
-                print ' ! The South developers regret this has happened, and would'
-                print ' ! like to gently persuade you to consider a slightly'
-                print ' ! easier-to-deal-with DBMS.'
+                print self.run_migration_error(migration)
             raise
         else:
             db.commit_transaction()
@@ -109,6 +116,9 @@ class Migrator(object):
         self.done_migrate(migration)
         self.send_ran_migration(migration)
         return result
+
+    def migrate_many(self, target, migrations):
+        raise NotImplementedError()
 
 
 class MigratorWrapper(object):
@@ -171,22 +181,52 @@ class FakeMigrator(MigratorWrapper):
         pass
 
 
+class LoadInitialDataMigrator(MigratorWrapper):
+    def load_initial_data(self, target):
+        if target != target.migrations[-1]:
+            return
+        # Load initial data, if we ended up at target
+        if self.verbosity:
+            print " - Loading initial data for %s." % target.app_name()
+        # Override Django's get_apps call temporarily to only load from the
+        # current app
+        old_get_apps = models.get_apps
+        models.get_apps = lambda: [models.get_app(target.app_name())]
+        try:
+            call_command('loaddata', 'initial_data', verbosity=self.verbosity)
+        finally:
+            models.get_apps = old_get_apps
+
+    def migrate_many(self, target, migrations):
+        migrator = self._migrator
+        result = migrator.__class__.migrate_many(migrator, target, migrations)
+        if result:
+            self.load_initial_data(target)
+        return True
+
+
 class Forwards(Migrator):
     """
     Runs the specified migration forwards, in order.
     """
     torun = 'forwards'
 
-    def status(self, migration):
+    @staticmethod
+    def title(target):
+        return " - Migrating forwards to %s." % target.name()
+
+    @staticmethod
+    def status(migration):
         return ' > %s' % migration
+
+    @staticmethod
+    def orm(migration):
+        return migration.orm()
 
     def forwards(self, migration):
         return self._wrap_direction(migration.forwards(), self.orm(migration))
 
     direction = forwards
-
-    def orm(self, migration):
-        return migration.orm()
 
     @staticmethod
     def record(migration):
@@ -195,6 +235,39 @@ class Forwards(Migrator):
         record.applied = datetime.datetime.utcnow()
         record.save()
 
+    def format_backwards(self, migration):
+        old_debug, old_dry_run = db.debug, db.dry_run
+        db.debug = db.dry_run = True
+        stdout = sys.stdout
+        sys.stdout = StringIO()
+        try:
+            self.backwards(migration)()
+            return sys.stdout.getvalue()
+        except:
+            raise
+        finally:
+            db.debug, db.dry_run = old_debug, old_dry_run
+            sys.stdout = stdout
+
+    def run_migration_error(self, migration, extra_info=''):
+        extra_info = ('\n'
+                      '! You *might* be able to recover with:'
+                      '%s'
+                      '%s' %
+                      (self.format_backwards(migration), extra_info))
+        return super(Forwards, self).run_migration_error(migration, extra_info)
+
+    def migrate_many(self, target, migrations):
+        try:
+            for migration in migrations:
+                result = self.migrate(migration)
+                if result is False: # The migrations errored, but nicely.
+                    return False
+        finally:
+            # Call any pending post_syncdb signals
+            db.send_pending_create_signals()
+        return True
+
 
 class Backwards(Migrator):
     """
@@ -202,10 +275,16 @@ class Backwards(Migrator):
     """
     torun = 'backwards'
 
-    def status(self, migration):
+    @staticmethod
+    def title(target):
+        return " - Migrating backwards to just after %s." % target.name()
+
+    @staticmethod
+    def status(migration):
         return ' < %s' % migration
 
-    def orm(self, migration):
+    @staticmethod
+    def orm(migration):
         return migration.prev_orm()
 
     direction = Migrator.backwards
@@ -216,5 +295,11 @@ class Backwards(Migrator):
         record = MigrationHistory.for_migration(migration)
         if record.id is not None:
             record.delete()
+
+    def migrate_many(self, target, migrations):
+        for migration in migrations:
+            self.migrate(migration)
+        return True
+
 
 
