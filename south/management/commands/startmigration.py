9f192d5884ac7b8526f50b5523a7a597be55869d
Andrew Godwin <andrew@aeracode.org>
2010-01-18 19:50:41 +0000

    Ignore Storages on file fields.

--- south/management/commands/startmigration.py
+++ south/management/commands/startmigration.py
@@ -784,7 +784,7 @@ def pprint_fields(fields):
 ### Output sanitisers
 
 
-USELESS_KEYWORDS = ["choices", "help_text", "upload_to", "verbose_name"]
+USELESS_KEYWORDS = ["choices", "help_text", "upload_to", "verbose_name", "storage"]
 USELESS_DB_KEYWORDS = ["related_name", "default"] # Important for ORM, not for DB.
 
 def remove_useless_attributes(field, db=False):
