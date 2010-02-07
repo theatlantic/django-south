cd09868420f107a0fa6f689db5d206ff0bf0ddca
Andrew Godwin <andrew@aeracode.org>
2010-02-07 19:31:39 +0000

    Add ability to ignore fields, and use this for a django-taggit plugin

--- south/introspection_plugins/__init__.py
+++ south/introspection_plugins/__init__.py
@@ -4,3 +4,4 @@
 # These imports trigger the lower-down files
 import south.introspection_plugins.geodjango
 import south.introspection_plugins.django_tagging
+import south.introspection_plugins.django_taggit
