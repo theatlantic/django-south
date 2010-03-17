8be2b65782fec1acc176b4f50b7ad870c2680ccc
Andrew Godwin <andrew@aeracode.org>
2010-03-17 20:16:13 +0000

    Fix multi-db recording of migrations

--- south/models.py
+++ south/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from south.db import DEFAULT_DB_ALIAS
 
 class MigrationHistory(models.Model):
     app_name = models.CharField(max_length=255)
@@ -6,13 +7,24 @@ class MigrationHistory(models.Model):
     applied = models.DateTimeField(blank=True)
 
     @classmethod
-    def for_migration(cls, migration):
+    def for_migration(cls, migration, database):
         try:
-            return cls.objects.get(app_name=migration.app_label(),
-                                   migration=migration.name())
+            # Switch on multi-db-ness
+            if database != DEFAULT_DB_ALIAS:
+                # Django 1.2
+                objects = cls.objects.using(database)
+            else:
+                # Django <= 1.1
+                objects = cls.objects
+            return objects.get(
+                app_name=migration.app_label(),
+                migration=migration.name(),
+            )
         except cls.DoesNotExist:
-            return cls(app_name=migration.app_label(),
-                       migration=migration.name())
+            return cls(
+                app_name=migration.app_label(),
+                migration=migration.name(),
+            )
 
     def get_migrations(self):
         from south.migration.base import Migrations
