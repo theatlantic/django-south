ef3b82dda7905538725b8afd545f9e46ac4dd8a9
Andrew Godwin <andrew@aeracode.org>
2010-03-25 20:06:32 +0000

    Version bump.

--- south/__init__.py
+++ south/__init__.py
@@ -2,7 +2,7 @@
 South - Useable migrations for Django apps
 """
 
-__version__ = "0.7-rc1"
+__version__ = "0.7"
 __authors__ = [
     "Andrew Godwin <andrew@aeracode.org>",
     "Andy McCurdy <andy@andymccurdy.com>"
