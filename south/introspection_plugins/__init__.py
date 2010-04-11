fed97273ba5a760ffdfade89cad998dec1058fa4
Andrew Godwin <andrew@aeracode.org>
2010-04-11 12:16:00 +0100

    Enable objectpermissions plugin

--- south/introspection_plugins/__init__.py
+++ south/introspection_plugins/__init__.py
@@ -5,3 +5,5 @@
 import south.introspection_plugins.geodjango
 import south.introspection_plugins.django_tagging
 import south.introspection_plugins.django_taggit
+import south.introspection_plugins.django_objectpermissions
+
