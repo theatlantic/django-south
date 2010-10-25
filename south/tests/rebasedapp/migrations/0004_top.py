3b0d04c955e73f95ef1e69655a8f8880dfdc6931
Andrew Godwin <andrew@aeracode.org>
2010-10-25 16:30:29 +0300

    branch:      rebase Actually add test app

---
+++ south/tests/rebasedapp/migrations/0004_top.py
@@ -0,0 +1,11 @@
+from south.db import db
+from south.v2 import SchemaMigration
+
+class Migration(SchemaMigration):
+
+    def forwards(self):
+        pass
+    
+    def backwards(self):
+        pass
+
