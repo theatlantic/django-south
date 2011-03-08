7885f95310a6ddb1a7f2bf145edc50cdbdeafa0f
Andrew Godwin <andrew@aeracode.org>
2011-03-08 21:33:24 +0000

    Merge

--- south/management/commands/schemamigration.py
+++ south/management/commands/schemamigration.py
@@ -178,15 +178,14 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
         %(forwards)s
 
-
     def backwards(self, orm):
         %(backwards)s
-
 
     models = %(frozen_models)s
 
