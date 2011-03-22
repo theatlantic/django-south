90e105230fa846f84bcbf0200e31d8f0cba1c555
Jeremy Dunck <jdunck@gmail.com>
2011-03-22 00:50:22 -0500

    migrationcheck command to clarify missing depends_on.

---
+++ south/management/commands/migrationcheck.py
@@ -0,0 +1,54 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.db.models import loading
+from django.test import simple
+
+from south.migration import Migrations
+from south.exceptions import NoMigrations
+from south.hacks import hacks
+
+class Command(BaseCommand):
+    help = "Runs migrations for each app in turn, detecting missing depends_on values."
+    usage_str = "Usage: ./manage.py migrationcheck"
+
+    def handle(self, **options):
+        runner = simple.DjangoTestSuiteRunner(verbosity=0)
+        err_msg = "Failed to migrate %s; see output for hints at missing dependencies:"
+        hacks.patch_flush_during_test_db_creation()
+        for app_name in settings.INSTALLED_APPS:
+            app_label = app_name.split(".")[-1]
+            if app_name == 'south':
+                continue
+
+            try:
+                Migrations(app_name)
+            except NoMigrations:
+                continue
+            app = loading.get_app(app_label)
+            print "processing %s" % app_name
+
+            old_config = runner.setup_databases()
+            try:
+                call_command('migrate', app_label, noinput=True, verbosity=0)
+                for model in loading.get_models(app):
+                    dummy = model._default_manager.exists()
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except Exception, e:
+                print err_msg % app_name
+                print e
+
+            finally:
+                runner.teardown_databases(old_config)
+#
+#for each app:
+#    start with blank db.
+#    syncdb only south (and contrib?)
+#
+#    migrate a single app all the way up.  any errors is missing depends_on.
+#    for all models of that app, try the default manager:
+#        from django.db.models import loading
+#        for m in loading.get_models(loading.get_app('a')):
+#            m._default_manager.exists()
+#    Any error is also a missing depends on.
